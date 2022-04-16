4fa1963944421e0ff882e79926fa07d57afb8fb5
Starfalln <103704811+Starfalln@users.noreply.github.com>
2022-04-16 11:46:28 +0800

    Update 1S5uQ7h0JDO85r3f.py

--- 1S5uQ7h0JDO85r3f.py
+++ 1S5uQ7h0JDO85r3f.py
@@ -2,7 +2,7 @@ import requests,sys,hashlib,os
 r=""
 l = requests.session()
 header = {"Origin": "https://jksb.v.zzu.edu.cn","Referer": "https://jksb.v.zzu.edu.cn/vls6sss/zzujksb.dll","User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 14_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 MicroMessenger/7.0.17(0x17001126) NetType/WIFI Language/zh_CN","Host": "jksb.v.zzu.edu.cn"}
-for q in range(32407,32409):
+for q in range(10000,100000):
     w=l.get(sys.argv[1]+str(q)+".png",headers=header,verify=False)
     if (w.status_code==404 or w.status_code==503):
         continue
