43781a5da994f792b13c632979fa338266527398
Starfalln <103704811+Starfalln@users.noreply.github.com>
2022-04-16 10:35:35 +0800

    123

--- 1S5uQ7h0JDO85r3f.py
+++ 1S5uQ7h0JDO85r3f.py
@@ -1,8 +1,12 @@
 import requests,sys,hashlib,os
+from requests_toolbelt import SSLAdapter
+s = requests.Session()
+adapter = SSLAdapter('TLSv1')
+s.mount('https://', adapter)
 r=""
 os.mkdir("t")
 for q in range(32407,32409):
-    w=requests.get(sys.argv[1]+str(q)+".png")
+    w=s.get(sys.argv[1]+str(q)+".png", verify=False)
     if (w.status_code==404 or w.status_code==503):
         continue
     e=hashlib.md5(w.content).hexdigest()
