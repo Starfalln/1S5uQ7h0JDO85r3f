cbbf2bc7a7ad444934d1d53f07277ffe6f0cea40
Starfalln <103704811+Starfalln@users.noreply.github.com>
2022-04-17 16:38:05 +0800

    123

--- 1S5uQ7h0JDO85r3f.py
+++ 1S5uQ7h0JDO85r3f.py
@@ -1,15 +1,13 @@
 import grequests,sys,hashlib,os,urllib3
 urllib3.disable_warnings()
 r=""
-u=[]
+us=[]
 task = []
 header = {"Origin": "https://jksb.v.zzu.edu.cn","Referer": "https://jksb.v.zzu.edu.cn/vls6sss/zzujksb.dll","User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 14_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 MicroMessenger/7.0.17(0x17001126) NetType/WIFI Language/zh_CN","Host": "jksb.v.zzu.edu.cn"}
 for a in range(10000,100000):
-    u.append(sys.argv[1]+str(a)+".png")
-for o in u:
-    req = grequests.request("get", url=o, headers=header)
-    task.append(req)
-resp = grequests.map(task, size=200)
+    us.append(sys.argv[1]+str(a)+".png")
+rs = (grequests.get(u,headers=header) for u in us)
+resp = grequests.map(rs, size=200)
 for q in range(10000,100000):
     if resp[q-10000]==None:
         continue
