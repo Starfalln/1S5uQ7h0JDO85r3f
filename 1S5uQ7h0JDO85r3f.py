b7640c083c657d408458efa7a5d3bd9af222c987
Starfalln <103704811+Starfalln@users.noreply.github.com>
2022-04-17 15:00:35 +0800

    123

--- 1S5uQ7h0JDO85r3f.py
+++ 1S5uQ7h0JDO85r3f.py
@@ -1,15 +1,19 @@
-import requests,sys,hashlib,os,urllib3
+import grequests,sys,hashlib,os,urllib3
 urllib3.disable_warnings()
 r=""
-l = requests.session()
+u=[]
+task = []
 header = {"Origin": "https://jksb.v.zzu.edu.cn","Referer": "https://jksb.v.zzu.edu.cn/vls6sss/zzujksb.dll","User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 14_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 MicroMessenger/7.0.17(0x17001126) NetType/WIFI Language/zh_CN","Host": "jksb.v.zzu.edu.cn"}
-for q in range(10000,100000):
-    w=l.get(sys.argv[1]+str(q)+".png",headers=header,verify=False)
-    if (w.status_code==404 or w.status_code==503):
+for a in range(10000,100000):
+    u.append(sys.argv[1]+str(a)+".png")
+for o in u:
+    req = grequests.request("get", url=o, headers=header)
+    task.append(req)
+resp = grequests.map(task, size=200)
+for q in resp:
+    if w.status_code==404:
         continue
     e=hashlib.md5(w.content).hexdigest()
     r=r+str(q)+","+e+"\n"
-    with open("./t/"+str(q)+".png","wb") as code:
-            code.write(w.content)
 with open("./t/123.csv","w") as d:
     d.write(r)
